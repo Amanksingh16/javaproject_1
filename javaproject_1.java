3a882a7c8be142f5fdcbf6c888363f1723a34f6e
rishikagupta2437 <rishikagupta2013@gmail,com>
2018-02-03 14:30:46 +0530

    some modifications

--- javaproject_1.java
+++ javaproject_1.java
@@ -1,104 +1,71 @@
 package javaproject_1;
 
 import java.io.*;
-import java.util.*;
+import java.io.File;
 
 public class javaproject_1 {
-
-	public static void main (String[] args) throws IOException {
+	
+	public static void main(String[] args) throws IOException {
 		
-		File file = new File("C:\\Users\\RISHIKA\\eclipse-workspace\\javaproject_1\\src\\javaproject_1\\abc.txt");
-		
-	 
-		FileReader in = null;
-		try {
-			in = new FileReader(file);             //reading file
-	int a;
-		while((a=in.read())!=-1) {
-			System.out.print((char)a);                      //printing the file of given path
-		}
-		
-		Scanner sc = new Scanner(file);
-		          String path = sc.nextLine();
-		                 File files= new File(path);              //user will input path
-		
-		File file1[]=files.listFiles();
-		
-		int count=0;                                    //to store the no. of files present
-		System.out.println();
-		System.out.println("files in the directory are:-");	
-			for(int i=0;i<file1.length;i++) {
-				if(file1[i].isFile())
-				 {
-					 count++;                                           //count the total numbers of files
-					 
-				 	 System.out.println(count+"."+"path and file name:- "+file1[i].getAbsolutePath());     //files of directory will print
-				 }
-				 
-			
+		 File file = new File("C:\\Users\\RISHIKA\\eclipse-workspace\\javaproject_1\\src\\javaproject_1\\abc.txt");
+		 
+		 FileReader in=null;
+		 try {
+			 in=new FileReader(file);
+			 
+			 int a;
+			 String path="";
+			 while((a=in.read())!=10) {
+				 path=path+(char)a;
 			 }
 			 
-			 check(file1 , path);        
-		
-		
-			
-		}
-		catch (Exception e){
-            System.out.print("error has occured");			
-		}
-	
-    finally {
-			if(in!=null)
-			{
-			in.close(); 
-			}
-			else {
-				
-			}
-			}
+			 //while((a=in.read())!=-1) {
+					//System.out.print((char)a); 
+			// }
+			 System.out.println();
+			 System.out.println(path);
+			 Listf(path);
+			 
+		 }
+		 catch(Exception e) {
+			 System.out.println("Error has occured");
+		 }
+		 finally {
+			 if(in!=null) {
+				 in.close();
+			 }
+		 }
 	}
-
-	public static void check(File file2[] , String path)            
 	
-	
-	                                                      /* will check if there will be any other directory present in the given directory
-	                                                       * if there then will print all the file of that second or whatever directory 
-	                                                       * or else print the file.
-	                                                       */
-
-{
-                                 int count = 0;
-             for(int i=0 ; i<file2.length; i++)   
-                    { 
-                                  if(file2[i].isDirectory())            //is there any other directory in the given directory
-                {
-                        File files2[] = file2[i].listFiles();
-                                        System.out.println();
-                                               System.out.println("folder name - "+file2[i].getName() + " // Folder Path - "+file2[i].getAbsolutePath());
-
-                      check(files2 , path);                 
-                               }
-                    else
-                                {
-                         if(file2[i].isFile())
-                               {
-                              count++;                    
-                                       }
-                        
-                         System.out.println(count+ "."+"path and file name:-"+file2[i].getAbsolutePath());			 
-		 				 
-                    }
-                    }
-}
-	}
-
-	
-		
-				
-				
+				 
 			
+		 
+		 public static void Listf(String Directory) {
+			 
+			 File file1 =new File(Directory); 
+			 
+			 File[] flist=file1.listFiles();
+			 
+			 for(int i=0;i<flist.length;i++) {
+				 File files= flist[i];
+				 if(files.isFile()) {
+					 
+					 System.out.println("file name"+files.getName()+" file path"+files.getAbsolutePath());
+					 
+				 }
+				 else if(files.isDirectory()) {
+					 Listf(files.getAbsolutePath());
+				 }
+			 }
+			 
+		 }
+	
 		
-		
-		
+		 
+
+}	 
 
 
+
+
+	
