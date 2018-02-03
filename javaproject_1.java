4389f0a86628c9c47ee011ff9289b65b1b30ebef
rishikagupta2437 <rishikagupta2013@gmail,com>
2018-02-03 20:37:10 +0530

    first submitt

--- javaproject_1.java
+++ javaproject_1.java
@@ -1,17 +1,27 @@
 package javaproject_1;
 
 import java.io.*;
+import java.util.Scanner;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import  java.lang.NullPointerException;
 
 public class javaproject_1 {
 	
 	public static void main(String[] args) throws IOException {
 		
-		 File file = new File("C:\\Users\\RISHIKA\\eclipse-workspace\\javaproject_1\\src\\javaproject_1\\abc.txt");
-		 
-		 FileReader in=null;
+		Scanner sc=new Scanner(System.in);           // user input
+		System.out.println("enter your path");     
+		String file =sc.nextLine();
+		
+		//File file = new File("C:\\Users\\RISHIKA\\eclipse-workspace\\javaproject_1\\src\\javaproject_1\\abc.txt");
+		                                   
+		
+		
+		 FileInputStream in=null;
 		 try {
-			 in=new FileReader(file);
+			 in=new FileInputStream(file);
 			 
 			 int a;
 			 String path="";
@@ -19,16 +29,13 @@ public class javaproject_1 {
 				 path=path+(char)a;
 			 }
 			 
-			 //while((a=in.read())!=-1) {
-					//System.out.print((char)a); 
-			// }
 			 System.out.println();
 			 System.out.println(path);
-			 Listf(path);
+			 Listf(path.substring(0,path.length()-1));
 			 
 		 }
 		 catch(Exception e) {
-			 System.out.println("Error has occured");
+			 System.out.println(e + "Error has occured");
 		 }
 		 finally {
 			 if(in!=null) {
@@ -50,7 +57,7 @@ public class javaproject_1 {
 				 File files= flist[i];
 				 if(files.isFile()) {
 					 
-					 System.out.println("file name"+files.getName()+" file path"+files.getAbsolutePath());
+					 System.out.println("file name:-"+files.getName()+"//// file path:-"+files.getAbsolutePath());
 					 
 				 }
 				 else if(files.isDirectory()) {
