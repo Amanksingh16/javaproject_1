9583bb8260d7e2f7e0035ce003fd286836e696fc
Aman Singh <35393404+Amanksingh16@users.noreply.github.com>
2018-02-05 05:36:17 -0800

    Update file_listing_app.java

--- file_listing_app.java
+++ file_listing_app.java
@@ -1,45 +1,54 @@
-package javaproject_1;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Iterator;
 
-public class file_listing_app {
+public class test {
 	
 	public static void main(String[] args) throws IOException {
 		
+		@SuppressWarnings("resource")
 		Scanner sc=new Scanner(System.in);           // user input
 		System.out.println("enter your path");     
-		String file =sc.nextLine();
+		String filepath =sc.nextLine();
 		
 		//File file = new File("C:\\Users\\RISHIKA\\eclipse-workspace\\javaproject_1\\src\\javaproject_1\\abc.txt");
 		                                   
-		
-		
+		File file = new File(filepath);
+		 
 		 FileInputStream in=null;
 		 
 		 try {
 			 in=new FileInputStream(file);
-			 
+			 int newline_count = 0;
 			 int a;
+			 String b = "" ;
+			 String c = "";
 			 String path="", path1="";
-			 while((a=in.read())!=10) {
-				 path=path+(char)a;
+			 while((a=in.read())!=-1) {
+				 if(a==10)
+				 {
+					 newline_count++;
+				 }
+				 if(newline_count == 0)
+				 {
+					 b = b + (char)a;
+					 path = b.substring(0, b.length()-1); 
+				 }
+				 if(newline_count == 1)
+				 {
+					 c = c + (char)a;	 
+				 }                 				    		
 			 }
+				path1 = c.substring(1, c.length()-1);
+
+			 File path2 = new File(path);            // The Path is fetched from the file
 			 
-			 System.out.println();
+			 File files[] = path2.listFiles();  
+			 
+			 Listf(files , path , path1);	
 			 System.out.println(path);
-			 Listf(path.substring(0,path.length()-1));
-			
-			while((a=in.read())!=-1) {
-				 path1=path1+(char)a;
-			 }
 			 System.out.println(path1);
-			 Listf(path1.substring(1,path1.length()-1));
-		 }
-		 catch(Exception e) {
-			System.out.println(e + "Error has occured");
 		 }
 		 finally {
 			 if(in!=null) {
@@ -48,36 +57,32 @@ public class file_listing_app {
 		 }
 	}
 	
-				 
-			
-		 
-		 public static void Listf(String Directory) throws IOException {
-			 
-			 String path1=new String();
-			 File fout =new File(path1);
+				
+		 public static void Listf(File files1[] , String path , String path1) throws IOException {
+			 			 
 			 ArrayList<File> List=new ArrayList<File>();
+			 Iterator<File> itr = null;
 			 BufferedWriter b= null;
-			  FileOutputStream fileout = new FileOutputStream(path1, true);
-			   b= new BufferedWriter(new OutputStreamWriter(fileout));
-			  
-			 File file1 =new File(Directory); 
-			 
-			 File[] flist=file1.listFiles();
-			 
-			 for(int i=0;i<flist.length;i++) {
-				 File files= flist[i];
-				 if(files.isFile()) {
-					 List.add(files);
+			 FileOutputStream fileout = null;
+			   
+			 for(int i=0;i<files1.length;i++) {
+				 if(files1[i].isFile()) {
+					 List.add(files1[i]);
 					// System.out.println("file name:-"+files.getName()+"//// file path:-"+files.getAbsolutePath());
 					 
 				 }
-				 else if(files.isDirectory()) {
-					 Listf(files.getAbsolutePath());
+				 else if(files1[i].isDirectory()) 
+				 {
+					 File files2[] = files1[i].listFiles();
+					 Listf(files2 , path , path1);
 				 }
 			 }
 			 try 
 			 {
-			 Iterator<File> itr= List.iterator();
+			   fileout =  new FileOutputStream(path1, true);
+			   b= new BufferedWriter(new OutputStreamWriter(fileout));
+				 	 
+			 itr= List.iterator();
 			 while(itr.hasNext()) {
 				 File f =itr.next();
 				 b.write("file name:-"+ f.getName());
